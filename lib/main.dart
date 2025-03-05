6fb10a70902a92d89cf7f1820a73930df9596662
steven <steven_1199@hotmail.es>
2025-03-04 21:38:03 -0600

    refactor: change App Title

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokédex GraphQL',
       theme: appTheme,
       home: const HomeScreen(),
     );
